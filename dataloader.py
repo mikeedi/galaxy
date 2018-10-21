f2b6f8ac95a77b22e28ec312176d39eb94b1dfa3
Mike Edukov <siredukov@gmail.com>
2018-10-21 23:31:06 +0300

    add third loader

--- dataloader.py
+++ dataloader.py
@@ -1,8 +1,10 @@
 import os
 
 from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data.dataset import Subset
 from torchvision import transforms, utils, datasets
 
+import numpy as np
 
 # mean and standart deviation of all dataset
 mean = [0.0804, 0.0667, 0.0513]
@@ -40,11 +42,18 @@ def dataloader(root='data/', batch_size=32, shuffle=True):
                                          transform=val_transform
                                         )
 
+    # create train evaluator
+    indices = np.arange(len(train_dataset))
+    random_indices = np.random.permutation(indices)[:len(val_dataset)]
+    train_subset = Subset(train_dataset, indices=random_indices)
+
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size,
                              shuffle=shuffle
                             )
     val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size,
-                             shuffle=False
+                             shuffle=shuffle, drop_last=True
                             )
-
-    return train_loader, val_loader
+    train_eval_loader = DataLoader(train_subset, batch_size=batch_size, 
+                             shuffle=shuffle, drop_last=True
+                            )
+    return train_loader, val_loader, train_eval_loader
