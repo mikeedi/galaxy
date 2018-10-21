b18fdd7b8a323ea6549d22ff18991d43481030c4
Mike Edukov <siredukov@gmail.com>
2018-10-21 22:15:54 +0300

    separate train and validation folder thus rewrite dataloader

--- dataloader.py
+++ dataloader.py
@@ -10,35 +10,41 @@ std = [0.1367, 0.1182, 0.0818]
 image_size = 264
 
 # all transformation
-data_transform = transforms.Compose([
+train_transform = transforms.Compose([
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(180),
         transforms.RandomVerticalFlip(),
+        transforms.ColorJitter(0, 0, 0, 0.1),
         transforms.Resize(256),        
         transforms.ToTensor(),
         transforms.Normalize(mean=mean,
                              std=std)
     ])
 
-def dataloader(root='data/', batch_size=32, shuffle=True, train_size=0.8):
+val_transform = transforms.Compose([
+        transforms.Resize(256),        
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean,
+                             std=std)
+    ])
+
+def dataloader(root='data/', batch_size=32, shuffle=True):
 
     if not os.path.exists(root):
         raise FileNotFoundError("image files was't found: ", root)
 
-    dataset = datasets.ImageFolder(root=root,
-                              transform=data_transform)
-
-    train_size = int(train_size * len(dataset))
-    test_size = len(dataset) - train_size
-    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    train_dataset = datasets.ImageFolder(root=root + 'train/',
+                                         transform=train_transform
+                                        )
+    val_dataset = datasets.ImageFolder(root=root + 'val/',
+                                         transform=val_transform
+                                        )
 
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size,
                              shuffle=shuffle
                             )
-    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size,
-                             shuffle=shuffle
+    val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size,
+                             shuffle=False
                             )
 
-    return train_loader, test_loader
-
-
+    return train_loader, val_loader
