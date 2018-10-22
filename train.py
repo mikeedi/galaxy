9463bf2676aab1717933d5caa6f5ddb81e8d6fad
Mike Edukov <siredukov@gmail.com>
2018-10-22 03:20:51 +0300

    rewrite boilerpalte with ignite

--- train.py
+++ train.py
@@ -2,108 +2,192 @@ import argparse
 import os
 
 from torch.utils.data import Dataset, DataLoader
+from torch.optim.lr_scheduler import ExponentialLR
 import torch.optim as optim
+from torch.optim import Adam, Adadelta, Adagrad, SparseAdam, \
+                        Adamax, SGD, RMSprop
 import torch.nn as nn
 import torch
+
+from ignite.engine import Engine, _prepare_batch, Events
+from ignite.metrics import Loss
+from ignite.handlers import ModelCheckpoint, EarlyStopping
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from Autoencoder import Autoencoder
-from dataloader import dataloader
+from dataloader import dataloader, train_transform
 from utils import show_image
 
-def train(root='data/', num_epochs=30, batch_size=64, use_cuda=True, code_size=64,
-                train_size=0.9, steps=20, save_images=True, save_path='Autoencoder.pt'):
 
-    if train_size > 1.:
-        raise ValueError("train_size could not be more 1: ", train_size)
-    
-    model = Autoencoder(code_size)
+def train(root='data/', num_epochs=30, batch_size=64, device='cuda', 
+        code_size=64, log_interval=20, save_images=True, 
+        name_optim='Adam', start_lr=0.001, gamma=0.9):    
+
+    model = Autoencoder(code_size).to(device)
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters())
+    optimizers = {
+        'Adam': Adam,
+        'Adadelta': Adadelta,
+        'Adagrad': Adagrad,
+        'SparseAdam': SparseAdam,
+        'Adamax': Adamax,
+        'SGD': SGD,
+        'RMSprop': RMSprop,
+    }
 
-    train_loss_save = []
-    test_loss_save = []
+    if name_optim not in optimizers:
+        raise ValueError('There is no such optimizer: {}'.format(name_optim))
 
-    if use_cuda:
-        model = model.cuda()
+    optimizer = optimizers[name_optim](model.parameters(), lr=start_lr)
+    lr_scheduler = ExponentialLR(optimizer, gamma=gamma)
 
-    train_loader, test_loader = dataloader(root=root, batch_size=batch_size, train_size=train_size)
+    train_loader, val_loader, train_eval_loader = dataloader(root=root, 
+                                                batch_size=batch_size)
 
-    print('Train-size: ', len(train_loader) * batch_size)
-    print('Test-size: ', len(test_loader) * batch_size)
-    print('------TRAIN-STARTED------')
-    for epoch in range(num_epochs):  # loop over the dataset multiple times
+    print('Train-size: ', len(train_loader.dataset))
+    print('Test-size: ', len(val_loader.dataset))
+    print('Train-eval-size: ', len(train_eval_loader.dataset))
 
-        for i, data in enumerate(train_loader, 0):
-    
-            running_loss = 0.0
-    
-            # get the inputs
-            inputs, _ = data
-            if use_cuda:
-                inputs = inputs.cuda()
+    def create_unsupervised_evaluator(model, metrics={}, device=None):
+        if device:
+            model.to(device)
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
+        def _inference(engine, batch):
+            model.eval()
+            with torch.no_grad():
+                x, _ = _prepare_batch(batch, device=device)
+                x_pred = model(x)
+                return x_pred, x
 
-            # forward + backward + optimize
-            outputs = model(inputs)
-            loss = criterion(outputs, inputs)
-            loss.backward()
-            optimizer.step()
+        engine = Engine(_inference)
 
-            # print statistics
-            running_loss += loss.item()
-            if i % steps == steps - 1:  # print every steps mini-batches
-                print('[{}, {}] training loss: {}'.format
-                          (epoch + 1, i + 1, running_loss/steps))
-        train_loss_save.append(running_loss / len(inputs))
+        for name, metric in metrics.items():
+            metric.attach(engine, name)
 
-        # if there is not test_data skip validation phase
-        if train_size == 1.:
-            continue
+        return engine
+
+    def process_function(engine, batch):
+        model.train()
+        optimizer.zero_grad()
+        x, _ = _prepare_batch(batch, device=device)
+        x_pred = model(x)
+        loss = criterion(x_pred, x)
+        loss.backward()
+        optimizer.step()
+        return loss.item() / len(batch)
+
+    trainer = Engine(process_function)
+
+    metrics = {
+    'avg_loss': Loss(criterion)
+    }
+    train_evaluator = create_unsupervised_evaluator(model, metrics=metrics, device=device)
+    val_evaluator = create_unsupervised_evaluator(model, metrics=metrics, device=device)
 
 
-        # validation step
-        with torch.no_grad():
-            for i, data in enumerate(test_loader, 0):
-                # get the inputs
-                inputs, _ = data
-                if use_cuda:
-                    inputs = inputs.cuda()
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def log_training_loss(engine):
+        iteration = (engine.state.iteration - 1) % len(train_loader) + 1
+        if iteration % log_interval == 0:
+            print("Epoch[{}] Iteration[{}/{}] Loss: {:.4f}"
+                  .format(engine.state.epoch, 
+                             iteration, 
+                             len(train_loader), 
+                             engine.state.output))
 
-                # forward only
-                outputs = model(inputs)
-                loss = criterion(outputs, inputs)
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def compute_and_display_offline_train_metrics(engine):
+        epoch = engine.state.epoch
+        print("Compute train metrics...")
+        metrics = train_evaluator.run(train_eval_loader).metrics
+        print("Training Results - Epoch: {}  Average Loss: {:.4f}"
+              .format(engine.state.epoch, 
+                          metrics['avg_loss']))
 
-                # print statistics
-                running_loss += loss.item()
-            test_loss_save.append(running_loss/len(test_loader))
-            print('validation loss: %.8f' % (train_loss_save[-1]))
-            if not os.path.exists('images/'):
-                os.mkdir('images/')
-            if save_images:
-                for i in np.random.random_integers(0, len(test_loader), 10):
-                    image, _ = test_loader.sampler.data_source.dataset[i]
-                    if use_cuda:
-                        image = image.cuda()
-                    plt.subplot(1, 2, 1)
-                    show_image(np.swapaxes(image.cpu().numpy(), 0, 2))
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def compute_and_display_val_metrics(engine):
+        epoch = engine.state.epoch
+        print("Compute validation metrics...")
+        metrics = val_evaluator.run(val_loader).metrics
+        print("Validation Results - Epoch: {}  Average Loss: {:.4f}"
+              .format(engine.state.epoch, 
+                          metrics['avg_loss']))
+
+    @trainer.on(Events.EPOCH_STARTED)
+    def update_lr_scheduler(engine):
+        lr_scheduler.step()
+        # Вывод значений скорости обучения:
+        if len(optimizer.param_groups) == 1:
+            lr = float(optimizer.param_groups[0]['lr'])
+            print("Learning rate: {}".format(lr))
+        else:
+            for i, param_group in enumerate(optimizer.param_groups):
+                lr = float(param_group['lr'])
+                print("Learning rate (group {}): {}".format(i, lr)) 
+
+    def score_function(engine):
+        val_avg_accuracy = engine.state.metrics['avg_loss']
+        return val_avg_accuracy
+
+    best_model_saver = ModelCheckpoint("best_models",  
+                                       filename_prefix="model",
+                                       score_name="val_accuracy",  
+                                       score_function=score_function,
+                                       n_saved=3,
+                                       save_as_state_dict=True,
+                                       create_dir=True)
+    val_evaluator.add_event_handler(Events.COMPLETED, 
+                                    best_model_saver, 
+                                    {"best_model": model})
+
+    training_saver = ModelCheckpoint("checkpoint",
+                                     filename_prefix="checkpoint",
+                                     save_interval=1000,
+                                     n_saved=1,
+                                     save_as_state_dict=True,
+                                     create_dir=True)
+    to_save = {
+        "model": model, 
+        "optimizer": optimizer, 
+        "lr_scheduler": lr_scheduler, 
+        'code_size': code_size,
+        'batch_size': batch_size,
+        'optimizer': name_optim,
+        'start_learning_rate': start_lr,
+        'gamma': gamma,
+        'train_transform': train_transform,
+    } 
+    trainer.add_event_handler(Events.ITERATION_COMPLETED, training_saver, to_save)
+
+    early_stopping = EarlyStopping(patience=10,     
+                                  score_function=score_function, 
+                                  trainer=trainer)
+    val_evaluator.add_event_handler(Events.EPOCH_COMPLETED, early_stopping)
+
+    if save_images:
+        if not os.path.exists('images/'):
+            os.mkdir('images/')
+        @trainer.on(Events.EPOCH_COMPLETED)
+        def save_before_and_after(engine):
+            model.eval()
+            epoch = engine.state.epoch
+            np.random.seed(1)
+            print('Test model on 10 images...')
+            for i in np.random.random_integers(0, len(val_loader), 10):
+                image, _ = val_loader.dataset[i]
+                image = image.to(device)
+                with torch.no_grad():
                     predict_image = model(image[None])[0].detach().cpu()
-                    plt.subplot(1, 2, 2)
-                    show_image(np.swapaxes(predict_image.numpy(), 0, 2))
-                    plt.savefig('images/{}_{}'.format(epoch, i))
-            torch.save(model, save_path+str(epoch))
-            if epoch > 0:
-                os.remove(save_path+str(epoch - 1))
+                plt.subplot(1, 2, 1)
+                show_image(np.swapaxes(image.cpu().numpy(), 0, 2))
+                plt.subplot(1, 2, 2)
+                show_image(np.swapaxes(predict_image.numpy(), 0, 2))
+                plt.savefig('images/{}_{}'.format(epoch, i))        
 
-
-    print('Finished Training')
-    print('Best train epoch: ', np.argmax(train_loss_save), np.max(train_loss_save))
-    print('Best test epoch: ', np.argmax(test_loss_save), np.max(test_loss_save))
-    torch.save(model, save_path)
-
+    trainer.run(train_loader, num_epochs)
+    print('THE-END')
 
 if __name__ == '__main__':
 
@@ -115,26 +199,24 @@ if __name__ == '__main__':
                         help='num iterations (default: 30)')
     parser.add_argument('--batch-size', type=int, default=64, metavar='BS',
                         help='batch size (default: 64)')
-    parser.add_argument('--use-cuda', type=bool, default=True, metavar='CUDA',
-                        help='use cuda (default: True)')
+    parser.add_argument('--device', type=str, default='cuda', metavar='CUDA',
+                        help='use cuda or cpu (default: cuda)')
     parser.add_argument('--code-size', type=int, default=64, metavar='CS',
                         help='code size (default: 64)')
-    parser.add_argument('--train-size', type=float, default=0.75, metavar='TS',
-                        help='train size (default: 0.75); between 0 to 1; if 1 then without validating')
-    parser.add_argument('--steps', type=int, default=20, metavar='ST',
-                        help='print statistics every steps (default: 200)')
+    parser.add_argument('--log-interval', type=int, default=20, metavar='LI',
+                        help='print statistics every log-interval (default: 20)')
     parser.add_argument('--save-images', type=bool, default=True, metavar='CUDA',
                         help='save images after and before autoencoder (default: True)')
 
     parser.register('type', bool, (lambda x: x.lower() in ("yes", "true", "t", "1")))
     args = parser.parse_args()
+
     root = args.root
     num_epochs = args.num_epochs
     batch_size = args.batch_size 
-    use_cuda = args.use_cuda
+    device = args.device
     code_size = args.code_size
-    train_size = args.train_size 
-    steps = args.steps
+    log_interval = args.log_interval
     save_images = args.save_images
-    train(root, num_epochs, batch_size, use_cuda, code_size,
-                    train_size, steps, save_images)
+    train(root, num_epochs, batch_size, device, code_size,
+                    log_interval)
