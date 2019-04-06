29ebba6d0fb8dd1fd51ad7a5eb3742a43ffd28d5
Mike Edukov <siredukov@gmail.com>
2019-04-06 19:52:24 +0300

    add validation part

--- vae_train.py
+++ vae_train.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 from torch import nn
 from dataloader import dataloader, train_transform
@@ -9,7 +11,9 @@ device = 'cuda:0'
 root = 'data/'
 start_lr = 0.001
 batch_size = 32
-
+log_interval = 50
+if not os.path.exists('images_vae/'):
+    os.mkdirs('images_vae/')
 
 train_loader, val_loader, _ = dataloader(root=root, 
                                             batch_size=batch_size)
@@ -24,6 +28,9 @@ optimizer = torch.optim.Adam(list(encoder.parameters()) + list(decoder.parameter
 num_epoch = 100
 
 for epoch in range(num_epoch):
+
+    encoder.train()
+    decoder.train()
     for i, (img, _) in enumerate(train_loader):
     
         optimizer.zero_grad()       
@@ -45,8 +52,44 @@ for epoch in range(num_epoch):
         loss = divergense_loss + reconstruct_loss
         loss.backward()
         optimizer.step()
-        print('-----------------')
-        print(loss.cpu().item())
-        print(divergense_loss.cpu().item())
-        print(reconstruct_loss.cpu().item())
+
+        if iteration % log_interval == 0:
+            print("Epoch[{}] Iteration[{}/{}] Loss: {:.4f}"
+                  .format(epoch, i, len(train_loader), loss))
         
+    with torch.no_grad():    
+        encoder.eval()
+        decoder.eval()
+        for i, (img, _) in enumerate(val_loader):
+                img = img.to(device)
+
+                # predict distribution
+                z_mean, z_log_var = encoder(img)
+
+                # sample z
+                eps = torch.randn((batch_size, latent_size)).to(device)
+                z_sample = z_mean + eps * torch.exp(z_log_var / 2)
+
+                # reconstruct image
+                decoded = decoder(z_sample)
+
+                divergense_loss = kl_divergence(z_mean, z_log_var)
+                reconstruct_loss = reconstruct_criterion(img, decoded)
+
+                loss = divergense_loss + reconstruct_loss
+
+                if iteration % log_interval == 0:
+                    print("Epoch[{}] Iteration[{}/{}] Loss: {:.4f}"
+                          .format(epoch, i, len(train_loader), loss))
+        
+        np.random.seed(46)
+        for i in np.random.random_integers(0, len(val_loader), 10):
+            image, _ = val_loader.dataset[i]
+            image = image.to(device)
+            predict_image = model(image[None])[0].detach().cpu()
+            plt.subplot(1, 2, 1)
+            show_image(np.swapaxes(image.cpu().numpy(), 0, 2))
+            plt.subplot(1, 2, 2)
+            show_image(np.swapaxes(predict_image.numpy(), 0, 2))
+            plt.savefig('images_vae/{}_{}'.format(epoch, i))        
+
