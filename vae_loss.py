5e716c6908bfff5702fc88f0fa1b728c6473368d
Mike Edukov <siredukov@gmail.com>
2019-04-12 17:32:59 +0300

    delete vae;

--- vae_loss.py
+++
@@ -1,8 +0,0 @@
-import torch
-from torch.nn.modules.loss import _Loss
-
-
-class VAE_Loss(_Loss):
-    
-    def forward(self, z_mean, z_log_var):
-        return -0.5 * (1 + z_log_var - z_mean**2 - torch.exp(z_log_var)).sum()
